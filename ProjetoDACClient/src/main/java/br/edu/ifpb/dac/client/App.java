c321e88d95ccead8b1636a8f7ed5ddefe9ad75b0
Diego Pessoa <diegopessoa12@gmail.com>
2019-12-12 11:30:10 -0300

    Ajuste no app client

--- ProjetoDACClient/src/main/java/br/edu/ifpb/dac/client/App.java
+++ ProjetoDACClient/src/main/java/br/edu/ifpb/dac/client/App.java
@@ -18,7 +18,8 @@ public class App
         System.out.println("Bem-vindos à Calculadora");
     	
         InitialContext ic = new InitialContext();
-        CalculadoraControllerRemote calculadoraController = (CalculadoraControllerRemote) ic.lookup("java:global/ProjetoDACEAR/ProjetoDACWeb-0.0.1-SNAPSHOT/CalculadoraController!br.edu.ifpb.dac.ejb.sessionbeans.CalculadoraControllerRemote");
+        CalculadoraControllerRemote calculadoraController = (CalculadoraControllerRemote) ic.
+        		lookup("java:global/ProjetoDACEAR/ProjetoDACWeb-0.0.1-SNAPSHOT/CalculadoraController!br.edu.ifpb.dac.ejb.sessionbeans.CalculadoraControllerRemote");
         
         System.out.println("SOMA = "+ calculadoraController.soma(5, 5));
         System.out.println("SUBTRACAO = "+ calculadoraController.subtrai(500, 90));
