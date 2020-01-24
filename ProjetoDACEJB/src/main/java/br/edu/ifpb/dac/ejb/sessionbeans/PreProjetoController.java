c0870c48adbb3c9a193aa656f8465ff37a3ecf21
Diones Gomes <diones.zack.dz@gmail.com>
2020-01-24 20:34:22 -0300

    Adicionando o controlador para gerenciar os pré Projetos, PreProjetoController.

---
+++ ProjetoDACEJB/src/main/java/br/edu/ifpb/dac/ejb/sessionbeans/PreProjetoController.java
@@ -0,0 +1,54 @@
+package br.edu.ifpb.dac.ejb.sessionbeans;
+
+import br.edu.ifpb.dac.ejb.dao.PreProjetoDao;
+import br.edu.ifpb.dac.ejb.entidades.PreProjeto;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+
+@RequestScoped
+@Named
+public class PreProjetoController implements Serializable {
+
+    private PreProjeto preProjeto;
+
+    @Inject
+    private PreProjetoDao preProjetoDAO;
+
+    public List<PreProjeto> buscarTodosOsPreProjetos(){
+        return preProjetoDAO.buscarTodos();
+    }
+
+    public String salvar(PreProjeto preProjeto){
+        preProjetoDAO.salvar(preProjeto);
+        return null;
+    }
+
+    /*                                              Criar esse método no DAO de aluno
+    public String remover((PreProjeto preProjeto){
+        this.preProjetoDAO.remover(preProjeto);
+        return null;
+    }
+     */
+
+    public String atualizar(PreProjeto preProjeto){
+        this.preProjetoDAO.atualizar(preProjeto);
+        return "??????"; // Definir para onde será redirecionado
+    }
+    public String buscarPreProjeto(Long id){
+        this.preProjetoDAO.buscar(id);
+        return "?????w"; // Definir para onde será redirecionado
+    }
+
+    public PreProjeto getPreProjeto() {
+        return preProjeto;
+    }
+
+    public void setPreProjeto(PreProjeto preProjeto) {
+        this.preProjeto = preProjeto;
+    }
+}
