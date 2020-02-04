035fdbafdad9df91a0d4a5d8e0677eb06a4f457d
Diones Gomes <diones.zack.dz@gmail.com>
2020-02-04 20:14:55 -0300

    Adicionando teste responsável por povoar a tabela de ALUNO no bando de dados.

--- ProjetoDACEJB/src/test/java/br/edu/ifpb/dac/ejb/AppTest.java
+++ ProjetoDACEJB/src/test/java/br/edu/ifpb/dac/ejb/AppTest.java
@@ -2,10 +2,20 @@ package br.edu.ifpb.dac.ejb;
 
 import static org.junit.Assert.assertTrue;
 
+import br.edu.ifpb.dac.ejb.dao.AlunoDao;
+import br.edu.ifpb.dac.ejb.dao.ProfessorDao;
+import br.edu.ifpb.dac.ejb.entidades.Aluno;
+import br.edu.ifpb.dac.ejb.entidades.Professor;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.runner.RunWith;
 
 import org.junit.Test;
+
+import javax.inject.Inject;
 
 /**
  * Unit test for simple App.
@@ -18,192 +28,49 @@ public class AppTest {
 		assertTrue(1==1);
 	}
 
-	//	@Inject
-//	AlunoDao alunoDao;
-//
-//	@Inject
-//	ProfessorDao professorDao;
-//
-//	@Inject
-//	PreProjetoDao preProjetoDao;
-//
-//	@Inject
-//	PeriodoDao periodoDao;
-//
-//	@Inject
-//	CriterioAvaliacaoDao criterioAvaliacaoDao;
-//
-//	@Inject
-//	TemaDao temaDao;
-//
-//	@Inject
-//	InscricaoDao inscricaoDao;
-//
-//	@Inject
-//	AvaliacaoDao avaliacaoDao;
-//
-//
-//	@PersistenceContext(unitName = "dac")
-//	EntityManager em;
-//
-//	@Deployment
-//	public static JavaArchive criarArquivoTeste() {
-//		return ShrinkWrap.create(JavaArchive.class)
-//				.addPackage(AlunoDao.class.getPackage())
-//				.addPackage(Aluno.class.getPackage())
-//				.addAsResource("META-INF/persistence.xml");
-//	}
-//
-//
-//	@Test
-//	public void testeSalvarAluno() {
-//		Aluno aluno = new Aluno();
-//		aluno.setNome("Tiringa");
-//		aluno.setEmail("tiringa@gmail.com");
-//		aluno.setMatricula("202022022020");
-//		aluno.setSenha("tiringa123");
-//		aluno.setCurso(Pessoa.Curso.ADS);
-//		alunoDao.salvar(aluno);
-//	}
-//
-//	@Test
-//	public void testeSalvarProfesssor() {
-//		Professor professor = new Professor();
-//		professor.setNome("lopim");
-//		professor.setEmail("lopim@gmail.com");
-//		professor.setMatricula("12345678");
-//		professor.setSenha("lopim123");
-//		professor.setCurso(Pessoa.Curso.ADS);
-//		professorDao.salvar(professor);
-//
-//	}
-//
-//	@Test
-//	public void testeSalvarPreProjeto(){
-//		PreProjeto preProjeto = new PreProjeto();
-//		preProjeto.setArquivo("arquivoPreProjeto");
-//		preProjeto.setDataEnvio(new Date());
-//		preProjetoDao.salvar(preProjeto);
-//	}
-//
-//	@Test
-//	public void testeSalvarPeriodo(){
-//		Periodo periodo = new Periodo();
-//		periodo.setDataInicio(new Date());
-//		periodo.setDataFim(new Date());
-//		periodo.setPeriodoLetivo("2020.1");
-//		periodoDao.salvar(periodo);
-//	}
-//
-//	@Test
-//	public void testeSalvarCriterioAvaliacao(){
-//		CriterioAvaliacao criterioAvaliacao = new CriterioAvaliacao();
-//		criterioAvaliacao.setCriterio("criterio");
-//		criterioAvaliacao.setNota(1.4f);
-//		criterioAvaliacaoDao.salvar(criterioAvaliacao);
-//
-//	}
-//
-//	@Test
-//	public void testeSalvarTema(){
-//		Tema tema = new Tema();
-//		tema.setTitulo("titulotema");
-//		tema.setDescricao("DescricaoTema");
-//		tema.setDisponivel(true);
-//
-//		Professor professor = new Professor();
-//		professor.setNome("professorTema");
-//		professor.setEmail("professorTema@gmail.com");
-//		professor.setMatricula("18294762");
-//		professor.setSenha("professorTema123");
-//		professor.setCurso(Pessoa.Curso.ENGENHARIA_AUTOMACAO);
-//		tema.setProfessor(professor);
-//
-//		temaDao.salvar(tema);
-//
-//	}
-//
-//	@Test
-//	public void testeSalvarAvaliacao(){
-//		Avaliacao avaliacao = new Avaliacao();
-//
-//		Professor professor = new Professor();
-//		professor.setNome("professorAvaliador");
-//		professor.setEmail("professorAvaliador@gmail.com");
-//		professor.setMatricula("18294762");
-//		professor.setSenha("professorAvaliador123");
-//		professor.setCurso(Pessoa.Curso.ADS);
-//		avaliacao.setAvaliador(professor);
-//
-//		avaliacao.setNotaGeral(7.0f);
-//
-//		CriterioAvaliacao criterioAvaliacao1 = new CriterioAvaliacao();
-//		criterioAvaliacao1.setCriterio("criterioAvaliacao");
-//		criterioAvaliacao1.setNota(1.8f);
-//
-//		avaliacao.setCriteriosAvaliacao(Arrays.asList(criterioAvaliacao1));
-//
-//		avaliacaoDao.salvar(avaliacao);
-//
-//
-//
-//
-//	}
-//
-//	@Test
-//	public void testeSalvarInscricao(){
-//		Inscricao inscricao = new Inscricao();
-//
-//		Aluno aluno = new Aluno();
-//		aluno.setNome("AlunoInscricao");
-//		aluno.setEmail("AlunoInscricao@gmail.com");
-//		aluno.setMatricula("202022022040");
-//		aluno.setSenha("AlunoInscricao123");
-//		aluno.setCurso(Pessoa.Curso.ADS);
-//
-//
-//		Professor professor = new Professor();
-//		professor.setNome("professorInscricao");
-//		professor.setEmail("professorInscricao@gmail.com");
-//		professor.setMatricula("02837493");
-//		professor.setSenha("professorInscricao123");
-//		professor.setCurso(Pessoa.Curso.ADS);
-//
-//		Tema tema = new Tema();
-//		tema.setTitulo("titulotemaInscricao");
-//		tema.setDescricao("DescricaotemaInscricao");
-//		tema.setDisponivel(true);
-//		tema.setProfessor(professor);
-//
-//
-//		inscricao.setDataInscricao(new Date());
-//
-//		Periodo periodo = new Periodo();
-//		periodo.setDataInicio(new Date());
-//		periodo.setDataFim(new Date());
-//		periodo.setPeriodoLetivo("2020.1");
-//
-//
-//		PreProjeto preProjeto = new PreProjeto();
-//		preProjeto.setArquivo("inscricaoPreProjeto");
-//		preProjeto.setDataEnvio(new Date());
-//
-//		inscricao.setNotaFinal(6.0f);
-//
-//
-//		Avaliacao avaliacaoInscricao = new Avaliacao();
-//		avaliacaoInscricao.setAvaliador(professor);
-//		avaliacaoInscricao.setNotaGeral(7.0f);
-//		CriterioAvaliacao criterioAvaliacao1 = new CriterioAvaliacao();
-//		criterioAvaliacao1.setCriterio("criterioAvaliacao");
-//		criterioAvaliacao1.setNota(1.8f);
-//		avaliacaoInscricao.setCriteriosAvaliacao(Arrays.asList(criterioAvaliacao1));
-//
-//		inscricao.setStatus(Inscricao.Status.CONTEMPLADO);
-//
-//		inscricaoDao.salvar(inscricao);
-//
-//	}
+	@Inject
+	AlunoDao prfessorDAO;
+
+	@Inject
+	ProfessorDao ProfessorDAO;
+
+	@Deployment
+	public static JavaArchive criarArquivoTeste() {
+		return ShrinkWrap.create(JavaArchive.class)
+				.addPackage(AlunoDao.class.getPackage())
+				.addPackage(Aluno.class.getPackage())
+				.addAsResource("META-INF/persistence.xml");
+	}
+
+	@Test
+	@InSequence(1)
+	public void testeSalvarAluno() {
+
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Antônio Alves");
+		aluno1.setMatricula("201612050570");
+		aluno1.setEmail("antonio@gmail.com");
+		aluno1.setCurso("ADS");
+		aluno1.setSenha("123");
+
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Jhonatan Leite");
+		aluno2.setMatricula("201612040430");
+		aluno2.setEmail("jhonatan@gmail.com");
+		aluno2.setCurso("ADS");
+		aluno2.setSenha("123");
 
 
+		Aluno aluno3 = new Aluno();
+		aluno3.setNome("Santiago Henrique");
+		aluno3.setMatricula("201612070760");
+		aluno3.setEmail("santiago@gmail.com");
+		aluno3.setCurso("ADS");
+		aluno3.setSenha("123");
+
+		prfessorDAO.salvar(aluno1);
+		prfessorDAO.salvar(aluno2);
+		prfessorDAO.salvar(aluno3);
+
+	}
 }
